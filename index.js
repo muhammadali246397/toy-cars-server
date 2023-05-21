a5701c6b274089a60096ba1ba2a7dc16b173926d
muhammadali246397 <muhammadali246397@gmail.com>
2023-05-21 11:18:06 +0600

    complete add fire base and post toy data

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port =process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('this server is running fine')
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rfaan6v.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const database = client.db('toyCars')
+    const carCollection = database.collection('carCollection')
+    // Connect the client to the server	(optional starting in v4.7)
+
+    app.post('/alltoys',async(req,res) => {
+      const toyinfo =req.body;
+      console.log(toyinfo)
+      const result = await carCollection.insertOne(toyinfo)
+      res.send(result)
+
+    })
+
+    app.get('/alltoys',async(req,res) => {
+      const cursor = carCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
